54a6971c41c229ccf3267b949d43be1c3de2a205
hoisharka <hoisharka@gmail.com>
2024-07-11 10:40:01 +0900

    테스트 주제 구분

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,20 +3,26 @@ const Home = () => {
   return (
     <div>
       <h1>UseEffectTest</h1>
-      <ul>
-        <li>
-          <a href={`/test01`}>01. window.addEventListener 의 이벤트 핸들러에선 state 갱신이 안됨</a>
-        </li>
-        <li>
-          <a href={`/test02`}>02. state에 대응되는 ref사용하여 대응 </a>
-        </li>
-        <li>
-          <a href={`/test03`}>03. useEffect 의존성으로 솔루션 모색</a>
-        </li>
-        <li>
-          <a href={`/test04`}>04. 핸들러를 ref로 관리</a>
-        </li>
-      </ul>
+
+      <div>
+        <h2>useEffect와 window.addEventListener</h2>
+        <ul>
+          <li>
+            <a href={`/test01`}>
+              01. window.addEventListener 의 이벤트 핸들러에선 state 갱신이 안됨
+            </a>
+          </li>
+          <li>
+            <a href={`/test02`}>02. state에 대응되는 ref사용하여 대응 </a>
+          </li>
+          <li>
+            <a href={`/test03`}>03. useEffect 의존성으로 솔루션 모색</a>
+          </li>
+          <li>
+            <a href={`/test04`}>04. 핸들러를 ref로 관리</a>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
